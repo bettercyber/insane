82fc0a0ed1f46b815e82a9a288afb8b756b47562
BetterCyber <78450870+bettercyber@users.noreply.github.com>
2024-01-17 07:59:44 -0600

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/no-name
+cd ~/insane
 ./ransomwatch.py scrape
 ./ransomwatch.py parse
 
@@ -12,4 +12,4 @@ git commit -m "updated by bot"
 git push origin master
 
 JSON=$(jq -r '.[].locations' groups.json | sed 's/"version": 3/"version": "3"/g' | sed 's/  //g' |  sed 's/"/\\\"/g' | tr -d '\n')
-cat ~/no-name/mutation.graphql | /usr/local/bin/gql-cli https://utjcezgxkfexda5vjmlq2g3nq4.appsync-api.us-west-2.amazonaws.com/graphql -H "x-api-key: $API_KEY" --transport appsync_http -V JSON:"$JSON"
+cat ~/insane/mutation.graphql | /usr/local/bin/gql-cli https://utjcezgxkfexda5vjmlq2g3nq4.appsync-api.us-west-2.amazonaws.com/graphql -H "x-api-key: $API_KEY" --transport appsync_http -V JSON:"$JSON"
